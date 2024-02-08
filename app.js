fdd4e968df187c31b28eb7f086c3c9f42638a0ba
ars-abs <ars@abstractit.in>
2024-02-08 15:42:39 +0530

    Create some coupons.

--- app.js
+++ app.js
@@ -5,14 +5,34 @@ const {site, api_key} = process.env
 
 chargebee.configure({ site, api_key })
 
-chargebee.subscription.list({
-  "status[is]": 'active',
-  "customer_id[is]": '6oadVU3azFV41fD'
-},).request(function(error,result) {
-  if(error){
-    console.log(error);
-  }
-  else{
+chargebee.subscription
+  .list({
+    "status[is]": 'active',
+    "customer_id[is]": '6oadVU3azFV41fD'
+  },)
+  .request(function(error,result) {
     console.log(result.list[0].subscription)
-  }
-});
+  });
+
+chargebee.item_family
+  .create({
+    id : "test-item-family",
+    description : "Test Item Family",
+    name : "Test Item Family"
+  })
+  .request(function(error,result) {
+    console.log(result);
+  });
+
+chargebee.coupon
+  .create_for_items({
+    id : "summer_offer",
+    name : "Summer Offer",
+    discount_percentage : "10.0",
+    discount_type : "PERCENTAGE",
+    duration_type : "FOREVER",
+    apply_on : "EACH_SPECIFIED_ITEM",
+  })
+  .request(function(error,result) {
+    console.log(result);
+  });
